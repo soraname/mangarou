9fc0b47a3a49437b63e09c882a4e981f27692286
p017406 <soranameart@gmail.com>
2019-04-30 18:12:43 -0300

    Tentando

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = true
+
+Vue.use(Vuex)
 
 new Vue({
   router,
